e51a2169afc611faecfbaf8dad8ca9c76cda050e
chinwizu <chinwianthony@gmail.com>
2022-10-06 09:29:10 +0100

    new branch feature

--- src/components/EventBindingTwo.js
+++ src/components/EventBindingTwo.js
@@ -6,7 +6,7 @@ class EventBindingTwo extends Component {
         super(props)
       
         this.state = {
-           message : "welcome arrow yeah"
+           message : "welcome arrow new branch"
         }
       }
 
